9aeabf4d4bc3d75c317a6fb95084d4a004549cc0
Elvis <elvis.e.amancio@gmail.com>
2025-06-11 00:13:41 -0300

    mudança pipeline

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, FirebaseApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
 
 import {
   Analytics,
